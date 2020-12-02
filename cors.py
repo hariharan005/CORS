64c64bacf5a8b81f822b14b87c902adbf73304bc
hariharan005 <43177885+hariharan005@users.noreply.github.com>
2020-12-02 19:25:30 +0530

    Update cors.py

--- cors.py
+++ cors.py
@@ -1,24 +1,20 @@
+import sys
 from requests import get
 
 cyan ='\033[36m'
 yellow ='\033[33m'
 white = '\033[37m'
 
-path = input("Enter the file path: ")
-print("")
-try:
-     with open(path) as f:
-            list = f.readlines()
-     list = [x.strip() for x in list]
+def action():
+        url = sys.argv[1]
+        response = get(url, headers={'User-Agent': 'Mozilla/5.0', 'Origin': 'ht>
+        status = response.headers
+        print("\n\n")
+        print(cyan, url)
+        print("\n")
+        print(white, status)
+        print("")
 
-     for url in list:
-             #payload = headers={'User-Agent': 'Mozilla/5.0', 'Origin': 'https://google.com'}
-             response = get(url, headers={'User-Agent': 'Mozilla/5.0', 'Origin': 'https://example.com'})
-             status = response.headers
-             print("\n\n")
-             print(cyan, url)
-             print("\n")
-             print(white, status)
-             print("")
-except:
-       print("Error")
+action()
+
+
